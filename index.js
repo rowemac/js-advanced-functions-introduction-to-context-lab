147bbbe50bc5dae277fda0b0cfd39137f2341a70
Mac Rowe <m.rowe42@gmail.com>
2021-06-04 10:53:04 -0400

    commit tree a320e4b22701353ac86ee9a3c5cb0c38a802eacb

--- index.js
+++ index.js
@@ -1,1 +1,81 @@
 // Your code here
+function createEmployeeRecord(array) {
+    let record = {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+
+    return record;
+}
+
+function createEmployeeRecords(arrayOfArrays) {
+    return arrayOfArrays.map(createEmployeeRecord);
+}
+
+function createTimeInEvent(employeeRecord, timeDateStamp ) {
+    let [ date, hour ] = timeDateStamp.split(' ')
+
+    employeeRecord.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour),
+        date: date
+    })
+    return employeeRecord;
+}
+
+function createTimeOutEvent(employeeRecord, timeDateStamp) {
+    let [ date, hour ] = timeDateStamp.split(' ')
+
+    employeeRecord.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour),
+        date: date
+    })
+    return employeeRecord;
+}
+
+function hoursWorkedOnDate(employee, theDate) {
+    let inDateTime = employee.timeInEvents.find(function(x){
+        return x.date === theDate
+    })
+
+    let outDateTime = employee.timeOutEvents.find(function(x){
+        return x.date === theDate
+    })
+
+    return (outDateTime.hour - inDateTime.hour) / 100;
+}
+
+function wagesEarnedOnDate(employee, theDate) {
+    let wages = hoursWorkedOnDate(employee, theDate) * employee.payPerHour
+    return wages
+}
+
+function allWagesFor(employee) {
+    
+    let daysWorked = employee.timeInEvents.map(function(x){
+        return x.date
+    })
+
+    let allWages = daysWorked.reduce(function(accumulator, currentValue){
+        return accumulator + wagesEarnedOnDate(employee, currentValue)
+    }, 0)
+
+    return allWages;
+
+}
+
+function calculatePayroll(arrayOfArrays) {
+    return arrayOfArrays.reduce(function(accumulator, currentValue){
+        return accumulator + allWagesFor(currentValue)
+    })
+}
+
+function findEmployeeByFirstName(sourceArray, firstName) {
+    let employee = sourceArray.find(firstName)
+    return employee
+}
